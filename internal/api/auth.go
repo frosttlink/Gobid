d50c51aa029ac5a9d393fc400f7b7dada17154d5
LeoClink77 <leolorena.alves@gmail.com>
2026-06-01 22:56:43 -0300

    feat: add CSRF protection and token retrieval functionality

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -4,7 +4,15 @@ import (
 	"net/http"
 
 	"github.com/frosttlink/gobid/internal/jsonutils"
+	"github.com/gorilla/csrf"
 )
+
+func (api *Api) HandleGetCSRFtoken(w http.ResponseWriter, r *http.Request) {
+	token := csrf.Token(r)
+	_ = jsonutils.Encondejson(w, r, http.StatusOK, map[string]any{
+		"csrf_token": token,
+	})
+}
 
 func (api *Api) AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
